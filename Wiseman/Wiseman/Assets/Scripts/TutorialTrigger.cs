95e2e0bee8bd8110e5845ce12fc9ef6ec3bcc1a9
Permilias <visseboulon.engrenaze@gmail.com>
2020-04-23 17:28:10 +0200

    LITERALLEMENT LE JEU VIDEO

---
+++ Wiseman/Wiseman/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public int tutorialIndex;
+    public float duration;
+
+    bool activated;
+
+    private void Update()
+    {
+        if (activated) return;
+
+        CheckActivation();
+    }
+
+    public void CheckActivation()
+    {
+        if(PlayerController.Instance.transform.position.z >= transform.position.z)
+        {
+            TutorialManager.Instance.PlayTutorial(tutorialIndex, duration);
+            activated = true;
+        }
+
+
+    }
+}
